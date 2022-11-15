000adf9b5d4a27f1e5e1bfd2ed0ce9be70056d4a
Aleksandra Klofat <a.klofat@gmail.com>
2022-11-15 13:06:45 +0100

    app first structure

---
+++ social_app.py
@@ -0,0 +1,18 @@
+# importing core packages
+import streamlit as st
+import pandas as pd
+
+
+
+
+# main app
+
+def main():
+	st.title("Visualisierungen")
+
+	
+       
+
+
+if __name__ == '__main__':
+	main()
